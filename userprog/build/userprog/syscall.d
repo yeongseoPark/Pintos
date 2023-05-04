c334f663db4f95577186fbbf0f146ffbc91e6b33
root <root@LAPTOP-MVO8EM6B.localdomain>
2023-05-04 19:23:58 +0900

    77/95

--- userprog/build/userprog/syscall.d
+++ userprog/build/userprog/syscall.d
@@ -5,7 +5,10 @@ userprog/syscall.o: ../../userprog/syscall.c \
  ../../include/lib/stdint.h ../../include/lib/kernel/stdio.h \
  ../../include/lib/syscall-nr.h ../../include/threads/interrupt.h \
  ../../include/threads/thread.h ../../include/lib/kernel/list.h \
- ../../include/threads/loader.h ../../include/userprog/gdt.h \
- ../../include/threads/flags.h ../../include/intrinsic.h \
- ../../include/threads/mmu.h ../../include/threads/pte.h \
- ../../include/threads/vaddr.h
+ ../../include/threads/synch.h ../../include/threads/loader.h \
+ ../../include/userprog/gdt.h ../../include/threads/flags.h \
+ ../../include/intrinsic.h ../../include/threads/mmu.h \
+ ../../include/threads/pte.h ../../include/threads/vaddr.h \
+ ../../include/filesys/filesys.h ../../include/filesys/off_t.h \
+ ../../include/filesys/file.h ../../include/userprog/process.h \
+ ../../include/threads/palloc.h
